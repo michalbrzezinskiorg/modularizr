87b20b8b520635f51501ac1001562d6bc2a9977b
Michal Brzezinski <michal@brzezinski.net>
2020-03-21 17:09:54 +0100

    interal calls security

--- src/main/java/com/decentralizer/spreadr/apiGateway/security/LoginClient.java
+++ src/main/java/com/decentralizer/spreadr/apiGateway/security/LoginClient.java
@@ -6,6 +6,7 @@ import com.decentralizer.spreadr.apiGateway.domain.RoleGateway;
 import com.decentralizer.spreadr.apiGateway.domain.UserGateway;
 import com.decentralizer.spreadr.modules.appconfig.AppconfigController;
 import com.decentralizer.spreadr.modules.appconfig.domain.Controller;
+import com.decentralizer.spreadr.modules.appconfig.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
@@ -14,39 +15,43 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.decentralizer.spreadr.SpreadrApplication.INSTANCE_ID;
+
 @Component
 @RequiredArgsConstructor
 class LoginClient {
 
     // todo: switch this implementation to some sort of http call
-    AppconfigController appconfigController;
-    ModelMapper modelMapper;
+    private final AppconfigController appconfigController;
+    private final ModelMapper modelMapper;
 
     public UserGateway getUserByLogin(String login) {
-        return modelMapper.map(appconfigController.getUserByLogin(login), UserGateway.class);
+        User userByLogin = appconfigController.getUserByLogin(login, INSTANCE_ID);
+        return modelMapper.map(userByLogin, UserGateway.class);
     }
 
     public List<PermissionGateway> findByPermissionFor(UserGateway user) {
-        return appconfigController.findByPermissionFor(user.getId())
+        return appconfigController.findByPermissionFor(user.getId(), INSTANCE_ID)
                 .stream()
                 .map(a -> modelMapper.map(a, PermissionGateway.class))
                 .collect(Collectors.toList());
     }
 
     public List<RoleGateway> findRolesByUser(UserGateway user) {
-        return appconfigController.findRolesByUser(user.getId())
+        return appconfigController.findRolesByUser(user.getId(), INSTANCE_ID)
                 .stream()
                 .map(a -> modelMapper.map(a, RoleGateway.class))
                 .collect(Collectors.toList());
     }
 
     public Set<ControllerGateway> findAllControllers() {
-        return appconfigController.findAllControllers().stream().map(
+        return appconfigController.findAllControllers(INSTANCE_ID).stream().map(
                 a -> modelMapper.map(a, ControllerGateway.class)
         ).collect(Collectors.toSet());
     }
 
     public void saveController(ControllerGateway controllerGateway) {
-        appconfigController.addNewControllerToDatabase(modelMapper.map(controllerGateway, Controller.class));
+        Controller map = modelMapper.map(controllerGateway, Controller.class);
+        appconfigController.addNewControllerToDatabase(map, INSTANCE_ID);
     }
 }
--- src/main/java/com/decentralizer/spreadr/modules/appconfig/AppconfigController.java
+++ src/main/java/com/decentralizer/spreadr/modules/appconfig/AppconfigController.java
@@ -5,12 +5,15 @@ import com.decentralizer.spreadr.modules.appconfig.domain.Permission;
 import com.decentralizer.spreadr.modules.appconfig.domain.Role;
 import com.decentralizer.spreadr.modules.appconfig.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AuthorizationServiceException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.HeaderParam;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+
+import static com.decentralizer.spreadr.SpreadrApplication.INSTANCE_ID;
 
 @RestController
 @RequestMapping("application")
@@ -20,29 +23,37 @@ public class AppconfigController {
     private final AppconfigService appconfigService;
 
     @GetMapping("controllers")
-    public Set<Controller> findAllControllers() {
+    public Set<Controller> findAllControllers(@HeaderParam("instance") String instance) {
         return appconfigService.findAllControllers();
     }
 
     @PostMapping("controllers")
-    public void addNewControllerToDatabase(Controller controller) {
+    public void addNewControllerToDatabase(Controller controller, @HeaderParam("instance") String instance) {
+        internalSecurity(instance);
         appconfigService.addNewControllerToDatabase(controller);
     }
 
     @GetMapping("user/{login}")
     public User getUserByLogin(@PathVariable String login, @HeaderParam("instance") String instance) {
-
+        internalSecurity(instance);
         return appconfigService.getUserByLogin(login);
     }
 
     @GetMapping("user/{id}/roles")
-    public List<Role> findRolesByUser(@PathVariable("id") UUID userId) {
+    public List<Role> findRolesByUser(@PathVariable("id") UUID userId, @HeaderParam("instance") String instance) {
+        internalSecurity(instance);
         return appconfigService.findRolesByUser(userId);
     }
 
     @GetMapping("user/{id}/permissions")
-    public List<Permission> findByPermissionFor(@PathVariable("id") UUID userId) {
+    public List<Permission> findByPermissionFor(@PathVariable("id") UUID userId, @HeaderParam("instance") String instance) {
+        internalSecurity(instance);
         return appconfigService.findByPermissionFor(userId);
+    }
+
+    private void internalSecurity(@HeaderParam("instance") String instance) {
+        if (!INSTANCE_ID.equals(instance))
+            throw new AuthorizationServiceException("instance id " + instance);
     }
 
 }
