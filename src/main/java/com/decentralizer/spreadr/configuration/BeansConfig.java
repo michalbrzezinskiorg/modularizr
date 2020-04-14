126a1c8b7f990fda82f2c1349417e1f4af6f1bb2
Michal Brzezinski <michal@brzezinski.net>
2020-04-14 18:35:28 +0200

    converters

--- src/main/java/com/decentralizer/spreadr/configuration/BeansConfig.java
+++ src/main/java/com/decentralizer/spreadr/configuration/BeansConfig.java
@@ -4,12 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import static com.decentralizer.spreadr.SpreadrApplication.INSTANCE_ID;
 
@@ -24,6 +29,25 @@ class BeansConfig {
         return WebClient.builder().defaultHeader("instance", INSTANCE_ID).filter(filter()).build();
     }
 
+    @Bean
+    public Converter<ZonedDateTime, LocalDateTime> zonedDateTime2LocalDateTimeConverter() {
+        return new Converter<>() {
+            @Override
+            public LocalDateTime convert(ZonedDateTime source) {
+                return source.toLocalDateTime();
+            }
+        };
+    }
+
+    @Bean
+    public Converter<LocalDateTime, ZonedDateTime> localDateTime2ZonedDateTimeConverter() {
+        return new Converter<>() {
+            @Override
+            public ZonedDateTime convert(LocalDateTime source) {
+                return source.atZone(ZoneId.systemDefault());
+            }
+        };
+    }
 
     @Bean
     public ModelMapper modelMapper() {
