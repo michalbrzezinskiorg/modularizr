03c884ed98f0fc2fe9cd6180c6b8e2f780dac29c
Michal Brzezinski <michal@brzezinski.net>
2020-04-19 16:23:47 +0200

    user without roles and permissions still can be valid login user. just no priv controllers to see...

--- src/main/java/com/decentralizer/spreadr/apigateway/security/SecurityUserDetailsService.java
+++ src/main/java/com/decentralizer/spreadr/apigateway/security/SecurityUserDetailsService.java
@@ -28,13 +28,13 @@ class SecurityUserDetailsService implements ReactiveUserDetailsService {
 
     @Override
     public Mono<UserDetails> findByUsername(String username) {
-        Mono<UserGatewayDTO> userByLogin = client.getUserByLogin(username);
-        Mono<Flux<RoleGatewayDTO>> roles = userByLogin.map(client::findRolesByUser);
-        Mono<Flux<PermissionGatewayDTO>> permissions = userByLogin.map(client::findByPermissionFor);
-        Mono<List<Set<ControllerGatewayDTO>>> f = getControllersFromRoles(roles);
-        Mono<List<Set<ControllerGatewayDTO>>> p = getControllersFromPermissions(permissions);
-        Flux<List<ControllerGatewayDTO>> map = getControllersList(f, p);
-        Flux<UserDetails> r = joinStreams(userByLogin, map);
+        Mono<UserGatewayDTO> user = client.getUserByLogin(username);
+        Mono<Flux<RoleGatewayDTO>> roles = user.map(client::findRolesByUser);
+        Mono<Flux<PermissionGatewayDTO>> permissions = user.map(client::findByPermissionFor);
+        Mono<List<Set<ControllerGatewayDTO>>> controllersFromRoles = getControllersFromRoles(roles);
+        Mono<List<Set<ControllerGatewayDTO>>> controllersFromPermissions = getControllersFromPermissions(permissions);
+        Flux<List<ControllerGatewayDTO>> controllers = getControllersList(controllersFromRoles, controllersFromPermissions);
+        Flux<UserDetails> r = joinStreams(user, controllers);
         return Mono.from(r);
     }
 
@@ -58,15 +58,22 @@ class SecurityUserDetailsService implements ReactiveUserDetailsService {
     }
 
     private Flux<UserDetails> joinStreams(Mono<UserGatewayDTO> userByLogin, Flux<List<ControllerGatewayDTO>> map) {
-        return map.zipWith(userByLogin).map(this::buildUser);
+        return map.zipWith(userByLogin).map(this::buildUser).switchIfEmpty(Flux.defer(() -> buildUser(userByLogin)));
+    }
+
+    private Mono<UserDetails> buildUser(Mono<UserGatewayDTO> userGatewayDTOMono) {
+        return userGatewayDTOMono.map(u -> User
+                .withUsername(u.getLogin())
+                .password(u.getPassword())
+                .build());
     }
 
     private UserDetails buildUser(Tuple2<List<ControllerGatewayDTO>, UserGatewayDTO> u) {
         return User
                 .withUsername(u.getT2().getLogin())
                 .password(u.getT2().getPassword())
-                .roles(getRoles(u)
-                ).build();
+                .roles(getRoles(u))
+                .build();
     }
 
     private String[] getRoles(Tuple2<List<ControllerGatewayDTO>, UserGatewayDTO> u) {
